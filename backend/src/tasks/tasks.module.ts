b89d2aa4f1042eaef792408ed96bb7aabbb7db98
iGewen <igewen@126.com>
2026-04-04 15:55:12 +0800

    fix: 修复TasksModule缺少RedisService依赖注入

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -4,6 +4,7 @@ import { TasksService } from './tasks.service';
 import { FeedModule } from '@/modules/feed/feed.module';
 import { OrderModule } from '@/modules/order/order.module';
 import { AdoptionModule } from '@/modules/adoption/adoption.module';
+import { RedisService } from '@/common/utils/redis.service';
 
 @Module({
   imports: [
@@ -12,6 +13,6 @@ import { AdoptionModule } from '@/modules/adoption/adoption.module';
     OrderModule,
     AdoptionModule,
   ],
-  providers: [TasksService],
+  providers: [TasksService, RedisService],
 })
 export class TasksModule {}
